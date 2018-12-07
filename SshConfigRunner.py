4b9d4d6a904db5cbe3a714c1f02edaddfcc72742
Moss <TMoss@msa.com>
2018-12-07 11:45:09 -0500

    SshConfig Runner

--- SshConfigRunner.py
+++ SshConfigRunner.py
@@ -2,6 +2,18 @@
 
 from netmiko import *
 
+class SshConnection:
+    def __init__(self, ip_add, d_type, username, password)
+        self.ip = ip_add
+        self.d_type = d_type #Device type
+        self.username = username
+        self.password = password
+
+    def ssh_conn():
+        pass
+    def tel_conn():
+        pass
+        
 def main():
     print("hooray running scripts")
 
