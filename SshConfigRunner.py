8353d071f15d4dbb76a7035888c3b12dd8e586ba
Moss <TMoss@msa.com>
2018-12-07 17:37:22 -0500

    Tested to run multiple commands on multiple devices with devices statically defined in script

--- SshConfigRunner.py
+++ SshConfigRunner.py
@@ -1,21 +1,48 @@
 #!/bin/bash python3
 
 from netmiko import *
+import getpass
 
-class SshConnection:
-    def __init__(self, ip_add, d_type, username, password)
-        self.ip = ip_add
-        self.d_type = d_type #Device type
-        self.username = username
-        self.password = password
 
-    def ssh_conn():
-        pass
-    def tel_conn():
-        pass
-        
+
+def ssh_connect(commands, **device):
+        ssh = ConnectHandler(**device)
+        if len(commands) > 1:
+
+            for command in commands:
+                output = ssh.send_command(command)
+                print(output)
+                print('\n***************************************\n')
+        else:
+            output = ssh.send_command(commands[0])
+            print(output)
+            print('\n***************************************\n')
+
+def telnet_connect(**device):
+    pass
+
 def main():
+
     print("hooray running scripts")
+
+    device1 = {
+    'device_type':'cisco_ios',
+    'ip':'172.30.186.11',
+    'username':'',
+    'password': ''
+    }
+
+    device2 = {
+    'device_type':'cisco_ios',
+    'ip':'172.30.187.66',
+    'username':'',
+    'password': ''
+    }
+
+    devices = [device1, device2]
+    commands = ['show interface status', 'show inventory']
+    for device in devices:
+        client = ssh_connect(commands, **device)
 
 if __name__ == '__main__':
     main()
