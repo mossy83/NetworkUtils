8663f228adcfd3d986adc111e2fd249240628daa
Moss <TMoss@msa.com>
2018-12-20 11:02:13 -0500

    Updating SshConfigRunner and the porgram flow

--- SshConfigRunner.py
+++ SshConfigRunner.py
@@ -1,7 +1,7 @@
 #!/bin/bash python3
 
-import getpass, ipaddress, os, netmiko, platform
-
+import ipaddress, os, netmiko, platform, json
+from getpass import getpass
 
 def test_ip(ipadd):
 
@@ -21,51 +21,38 @@ def test_ip(ipadd):
 def ssh_connect(commands, **device):
         ssh = netmiko.ConnectHandler(**device)
 
-        if len(commands) > 1:
-            print('Sending Multiple Commands')
-
-            for command in commands:
-                    output = ssh.send_command(command)
-                    print('\n' + command)
-                    print('\n#####################################\n')
-                    print(output)
-                    print('\n#####################################\n')
-
-        else:
-            output = ssh.send_command(commands[0])
-            print(commands[0])
-            print('\n#####################################\n')
-            print(output)
-            print('\n#####################################\n')
+        for command in commands:
+            if 'show' in command:
+                output = ssh.send_command(command)
+                print('\n' + command)
+                print('\n#####################################\n')
+                print(output)
+                print('\n#####################################\n')
+            else:
+                print('Using config mode')
 
 def telnet_connect(**device):
     pass
 
 def main():
     username = input('Enter your username: ')
-    password = getpass.getpass('Enter your password: ')
+    password = getpass('Enter your password: ')
 
-    ip_add_list = input('IP Address List - Filename and extension: ' )
+    device_list = input('IP Address List - Json Formatted list: ' )
     command_list = input('Command list- Filename and extension: ')
-    '''
-    device1 = {
-    'device_type':'cisco_ios',
-    'ip':'172.30.186.11',
-    'username':username,
-    'password': password
-    }
 
-    device2 = {
-    'device_type':'cisco_ios',
-    'ip':'172.30.187.66',
-    'username': username,
-    'password': password
-    }
 
-    devices = [device1, device2]
-    commands = ['show inventory', 'show interface status']
-    '''
+    with open(device_list) as devices_json:
+        devices = json.load(devices_json)
+
+    with open(command_list) as cmd_list:
+        commands = cmd_list.readlines()
+
+    print(type(commands))
     for device in devices:
+        device['username'] = username
+        device['password'] = password
+
         print('Attempting to connect to %s' %device['ip'])
         print('######################################')
         try:
